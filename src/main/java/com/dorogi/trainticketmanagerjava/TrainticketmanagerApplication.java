d93a3776c0b7ba63e00157025ce7716e86115d7d
Adam Dorogi <dorogi.zr@gmail.com>
2024-04-29 17:04:05 +0200

    Base project with Spring Initializr, and some settings

---
+++ src/main/java/com/dorogi/trainticketmanagerjava/TrainticketmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.dorogi.trainticketmanagerjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainticketmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainticketmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dorogi/trainticketmanagerjava/TrainticketmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dorogi.trainticketmanagerjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainticketmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
